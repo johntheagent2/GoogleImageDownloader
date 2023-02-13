53aa17117bfc122a164a7a5a78863c9f3995413f
johntheagent2 <caophat113@gmail.com>
2023-02-13 21:06:14 +0700

    a project which allows to download first 20 pictures on google

--- src/main/java/org/example/WebScrapper.java
+++ src/main/java/org/example/WebScrapper.java
@@ -5,43 +5,60 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WebScrapper {
+    static String url = "https://www.google.com.vn/search?q=nothing&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj5t8vRyJL9AhVer1YBHTaxA2EQ_AUoAXoECAEQAw&biw=1920&bih=947";
     public static void main(String[] args) {
-        int count = 1;
-        final String url = "https://www.google.com.vn/search?q=pikachu&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj5t8vRyJL9AhVer1YBHTaxA2EQ_AUoAXoECAEQAw&biw=1920&bih=947";
-
+        getDesireImage();
         try {
             final Document document = Jsoup.connect(url).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36").get();
-
             Elements e = document.getElementsByTag("img");
+
             for (Element a : e) {
-                String imgLink = String.valueOf(a.select(".yWs4tf"));
-
-                Pattern pattern = Pattern.compile("<img class=\"yWs4tf\" alt=\"\" src=\"([^\"]*([^\"]*(?:[^\\\\\"]|\\\\\\\\|\\\\\")*)+)\">", Pattern.CASE_INSENSITIVE);
-
-                Matcher matcher = pattern.matcher(imgLink);
+                Matcher matcher = analyseLinkForDownloadable(a);
                 if (matcher.matches()) {
-                    Path output = Paths.get("C:\\Users\\caoph\\Desktop\\Download\\"+Math.random()+".png");
-                    try (InputStream inputStream = new URL(matcher.group(1)).openStream()){
-                        Files.copy(inputStream, output, StandardCopyOption.REPLACE_EXISTING);
-                    } finally{
-                        System.out.println("DONE!!!");
-                    }
+                    saveImage(matcher);
                 }
-                count++;
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public static void getDesireImage(){
+        System.out.println("What do you want to download: ");
+
+        Scanner sc = new Scanner(System.in);
+        String string = sc.nextLine();
+
+        url = url.replace("nothing", string.replace(" ", "+").toLowerCase());
+    }
+
+    public static Matcher analyseLinkForDownloadable(Element a){
+        String imgLink = String.valueOf(a.select(".yWs4tf"));
+        Pattern pattern = Pattern.compile("<img class=\"yWs4tf\" alt=\"\" src=\"([^\"]*([^\"]*(?:[^\\\\\"]|\\\\\\\\|\\\\\")*)+)\">", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(imgLink);
+        return matcher;
+    }
+
+    public static void saveImage(Matcher matcher) throws IOException {
+        Path output = Paths.get("C:\\Users\\caoph\\Desktop\\Download\\"+Math.random()+".png");
+        try (InputStream inputStream = new URL(matcher.group(1)).openStream()){
+            Files.copy(inputStream, output, StandardCopyOption.REPLACE_EXISTING);
+        } finally{
+            System.out.println("DONE!!!");
+        }
+    }
+
 }
