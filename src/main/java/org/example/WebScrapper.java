d1645410b0066564b047b37516ae34872d0493a2
johntheagent2 <caophat113@gmail.com>
2023-02-13 21:14:59 +0700

    a project which allows to download first 20 pictures on google

--- src/main/java/org/example/WebScrapper.java
+++ src/main/java/org/example/WebScrapper.java
@@ -18,16 +18,18 @@ import java.util.regex.Pattern;
 
 public class WebScrapper {
     static String url = "https://www.google.com.vn/search?q=nothing&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj5t8vRyJL9AhVer1YBHTaxA2EQ_AUoAXoECAEQAw&biw=1920&bih=947";
+    static String name;
     public static void main(String[] args) {
         getDesireImage();
         try {
             final Document document = Jsoup.connect(url).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36").get();
             Elements e = document.getElementsByTag("img");
-
+            int number = 0;
             for (Element a : e) {
                 Matcher matcher = analyseLinkForDownloadable(a);
                 if (matcher.matches()) {
-                    saveImage(matcher);
+                    saveImage(matcher,number);
+                    number++;
                 }
             }
 
@@ -40,9 +42,9 @@ public class WebScrapper {
         System.out.println("What do you want to download: ");
 
         Scanner sc = new Scanner(System.in);
-        String string = sc.nextLine();
+        name = sc.nextLine();
 
-        url = url.replace("nothing", string.replace(" ", "+").toLowerCase());
+        url = url.replace("nothing", name.replace(" ", "+").toLowerCase());
     }
 
     public static Matcher analyseLinkForDownloadable(Element a){
@@ -52,8 +54,8 @@ public class WebScrapper {
         return matcher;
     }
 
-    public static void saveImage(Matcher matcher) throws IOException {
-        Path output = Paths.get("C:\\Users\\caoph\\Desktop\\Download\\"+Math.random()+".png");
+    public static void saveImage(Matcher matcher, int number) throws IOException {
+        Path output = Paths.get("C:\\Users\\caoph\\Desktop\\Download\\"+number+"_"+name+".png");
         try (InputStream inputStream = new URL(matcher.group(1)).openStream()){
             Files.copy(inputStream, output, StandardCopyOption.REPLACE_EXISTING);
         } finally{
